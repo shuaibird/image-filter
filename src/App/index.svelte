8073fdfd31c1b829807fcbfbe84bdfd199faae18
shuaibird <shuaibird@icloud.com>
2021-01-14 14:25:05 +0800

    init

---
+++ src/App/index.svelte
@@ -0,0 +1,12 @@
+<div>
+  <label class="file">
+    <input type="file" id="file" aria-label="File" accept="image/*">
+    <span class="file-custom"></span>
+  </label>
+</div>
+
+<div class="select">
+  <select aria-label="Select image filter">
+    <option selected>Choose a filter</option>
+  </select>
+</div>
